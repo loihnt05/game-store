fe19f0a65fbba3c19ee6bcf9047142370503d5eb
loihnt05 <23520869@gm.uit.edu.vn>
2025-12-07 13:29:02 +0700

    handle route + invalid input

---
+++ GameStore.Api/Endpoints/GamesEndpoints.cs
@@ -0,0 +1,71 @@
+using GameStore.Api.Dtos;
+using Microsoft.VisualBasic;
+
+namespace GameStore.Api.Endpoints;
+
+public static class GamesEndpoints
+{
+    const string GetGameEndpointName = "GetGameById";
+
+    private static readonly List<GameDto> games = new()
+    {
+        new GameDto(1, "The Witcher 3: Wild Hunt", "RPG", 39.99m, new DateOnly(2015, 5, 19)),
+        new GameDto(2, "Cyberpunk 2077", "RPG", 59.99m, new DateOnly(2020, 12, 10)),
+        new GameDto(3, "Minecraft", "Sandbox", 26.95m, new DateOnly(2011, 11, 18)),
+        new GameDto(4, "Among Us", "Party", 4.99m, new DateOnly(2018, 6, 15)),
+        new GameDto(5, "Hades", "Roguelike", 24.99m, new DateOnly(2020, 9, 17))
+    };
+
+    public static RouteGroupBuilder MapGamesEndpoints(this WebApplication app)
+    {
+        var group = app.MapGroup("games")
+                       .WithParameterValidation();
+        group.MapGet("/", () => games);
+
+        group.MapGet("/{id}", (int id) =>
+        {
+            GameDto? game = games.Find(game => game.Id == id);
+            return game is null ? Results.NotFound() : Results.Ok(game);
+        });
+        group.MapPost("/", (CreateGameDto newGame) =>
+        {
+            GameDto gameDto = new(
+                games.Count + 1,
+                newGame.Name,
+                newGame.Genre,
+                newGame.Price,
+                newGame.ReleaseDate
+            );
+            games.Add(gameDto);
+            return Results.CreatedAtRoute(GetGameEndpointName, new { id = gameDto.Id }, gameDto);
+        });
+
+        group.MapPost("/{id}", (int id, UpdateGameDto updateGame) =>
+        {
+            var index = games.FindIndex(game => game.Id == id);
+            if (index == -1)
+            {
+                return Results.NotFound();
+            }
+
+            games[index] = new GameDto(
+                id,
+                updateGame.Name,
+                updateGame.Genre,
+                updateGame.Price,
+                updateGame.ReleaseDate
+            );
+
+            return Results.NoContent();
+        });
+
+        group.MapDelete("/{id}", (int id) =>
+        {
+            games.RemoveAll(game => game.Id == id);
+            return Results.NoContent();
+        });
+
+        return group;
+    }
+
+}
--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,58 +1,8 @@
-using GameStore.Api.Dtos;
+using GameStore.Api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-string GetGameEndpointName = "GetGameById";
-
-List<GameDto> games = new()
-{
-    new GameDto(1, "The Witcher 3: Wild Hunt", "RPG", 39.99m, new DateOnly(2015, 5, 19)),
-    new GameDto(2, "Cyberpunk 2077", "RPG", 59.99m, new DateOnly(2020, 12, 10)),
-    new GameDto(3, "Minecraft", "Sandbox", 26.95m, new DateOnly(2011, 11, 18)),
-    new GameDto(4, "Among Us", "Party", 4.99m, new DateOnly(2018, 6, 15)),
-    new GameDto(5, "Hades", "Roguelike", 24.99m, new DateOnly(2020, 9, 17))
-};
-
-app.MapGet("games", () => games);
-
-app.MapGet("games/{id}", (int id) => {
-    GameDto? game = games.Find(game => game.Id == id);
-    return game is null ? Results.NotFound() : Results.Ok(game);
-});
-app.MapPost("games", (CreateGameDto newGame) => 
-{
-    GameDto gameDto = new(
-        games.Count + 1,
-        newGame.Name,
-        newGame.Genre,
-        newGame.Price,
-        newGame.ReleaseDate
-    );
-    games.Add(gameDto);
-    return Results.CreatedAtRoute(GetGameEndpointName, new { id = gameDto.Id }, gameDto);
-});
-
-app.MapPut("games/{id}", (int id, UpdateGameDto updateGame) => {
-    var index = games.FindIndex(game => game.Id == id);
-    if (index == -1) {
-        return Results.NotFound();
-    }
-    
-    games[index] = new GameDto(
-        id,
-        updateGame.Name,
-        updateGame.Genre, 
-        updateGame.Price,
-        updateGame.ReleaseDate
-    );
-
-    return Results.NoContent();
-});
-
-app.MapDelete("games/{id}", (int id) => {
-    games.RemoveAll(game => game.Id == id);
-    return Results.NoContent(); 
-});
+app.MapGamesEndpoints();
 
 app.Run();
